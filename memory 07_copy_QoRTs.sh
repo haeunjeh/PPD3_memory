8f67bbe9a7103dc6f007d27df25e00801fe2d60f
haeunjeh <59022705+haeunjeh@users.noreply.github.com>
2020-01-07 15:19:42 -0500

    Create memory 07_copy_QoRTs.sh

---
+++ memory 07_copy_QoRTs.sh
@@ -0,0 +1,25 @@
+BASE_DIR=/data5/huj181/PPD3_memory/QoRTs_out
+OUT_DIR=/data5/huj181/PPD3_memory/DEG
+cp ${BASE_DIR}/WT1/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/WT1.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/WT2/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/WT2.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/WT3/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/WT3.QC.geneCounts.formatted.for.DESeq.txt.gz
+
+cp ${BASE_DIR}/T+_1/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T1.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/T+_2/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T2.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/T+_3/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T3.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/T+_4/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T4.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/T+_5/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T5.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/T+_6/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/T6.QC.geneCounts.formatted.for.DESeq.txt.gz
+
+cp ${BASE_DIR}/Gen1_1/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen1_1.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen1_2/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen1_2.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen1_3/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen1_3.QC.geneCounts.formatted.for.DESeq.txt.gz
+
+cp ${BASE_DIR}/Gen2_1/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_1.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_2/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_2.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_3/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_3.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_4/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_4.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_5/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_5.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_6/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_6.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_7/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_7.QC.geneCounts.formatted.for.DESeq.txt.gz
+cp ${BASE_DIR}/Gen2_8/QC.geneCounts.formatted.for.DESeq.txt.gz ${OUT_DIR}/Gen2_8.QC.geneCounts.formatted.for.DESeq.txt.gz
